128e819f3138fb0afa29ec6ded73eb38ae526d4e
Christopher Coballes <cobecoballes@gmail.com>
2020-05-18 02:03:33 -0400

    Update xentrino-firmware.ino

--- xentrino-firmware.ino
+++ xentrino-firmware.ino
@@ -15,7 +15,6 @@ PSCoE-Computer Engineering Society.
 
 */
 
-//ROS headers
 #if (ARDUINO >= 100)
  #include <Arduino.h>
 #else
@@ -37,27 +36,17 @@ PSCoE-Computer Engineering Society.
 
 // #define ENCODER_OPTIMIZE_INTERRUPTS // comment this out on Non-Teensy boards
 
-#define COMMAND_RATE 20 //hz
-#define DEBUG_RATE 5
-/* 
-typedef enum robot
+struct robotFrame
 {
-int weel_diameter
-int Lbase;
-int Xbase;
-}  p, y;
-*/
 
-//struct robot
-//{
-//base robot_base
- int max_rpm_;
- float wheels_x_distance_;
- float wheels_y_distance_;
+ int max_rpm;
+ float wheels_x_distance;
+ float wheels_y_distance;
  float pwm_res_;
- float wheel_circumference_;
- int total_wheels_;
-//};
+ float wheel_circumference;
+ int total_wheels;
+};
+
 struct port
 {
  int pwm_pin;
@@ -88,19 +77,29 @@ struct pwm
   int motor4;
   };
   
-float g_req_linear_vel_x = 0;
-float g_req_linear_vel_y = 0;
-float g_req_angular_vel_z = 0;
-unsigned long g_prev_command_time = 0;
-unsigned long prev_debug_time  = 0;
-int counts_per_rev_  = 0;
-unsigned long prev_update_time_  = 0;
-long prev_encoder_ticks_  = 0;
 
-
+struct PID 
+{
 float Kp =   0.5;
 float Kd =   0;
 float Ki =   0;
+};
+
+
+#define COMMAND_RATE 20 //hz
+#define DEBUG_RATE 5
+
+float g_req_linear_vel_x = 0;
+float g_req_linear_vel_y = 0;
+float g_req_angular_vel_z = 0;
+
+unsigned long g_prev_command_time = 0;
+static unsigned long prev_control_time = 0;
+
+static unsigned long prev_debug_time = 0;
+unsigned long  prev_encoder_time;
+double  prev_encoder_ticks;
+
 
 Encoder encoder1(2, 17);
 Encoder encoder2(3, 15);
@@ -166,14 +165,11 @@ void publishRPM(unsigned long time) {
 }
 */
 
-void twist_to_cmd_RPM( const geometry_msgs::Twist& cmd_msg) {
-  double g_req_linear_vel_x  = cmd_msg.linear.x;
-  double g_req_linear_vel_y = cmd_msg.angular.z;
-  double g_req_angular_vel_z  = cmd_msg.angular.y;  // zero
- g_prev_command_time = millis();
-}
 
-
+/*------------------------------------------------------------------------
+ * 
+ * 
+ -------------------------------------------------------------------------*/
 rpm  IF_kinematics_RPM( float   linear_Vx, float linear_Vy , float angular_Vz)
 {
   int rpm_req1;
@@ -200,8 +196,16 @@ if (angular_Vz == 0) {     // go straight
   // return  rpm ;
 }
 
+void twist_to_cmd_RPM( const geometry_msgs::Twist& cmd_msg) {
+  double g_req_linear_vel_x  = cmd_msg.linear.x;
+  double g_req_linear_vel_y = cmd_msg.angular.z;
+  double g_req_angular_vel_z  = cmd_msg.angular.y;  // zero
+ g_prev_command_time = millis();
+}
+
 velocities cmd_to_twist_VEL( int rpm1,  int rpm2)
  {
+   struct robotFrame frame;
    struct velocities vel;
    //struct vel;
     float average_rps_x;
@@ -209,21 +213,25 @@ velocities cmd_to_twist_VEL( int rpm1,  int rpm2)
     float average_rps_a;
 
     average_rps_x = ((float)(rpm1 + rpm2 ) / 2) / 60; // RPM
-    vel.linear_x = average_rps_x * wheel_circumference_ ; // m/s
+    vel.linear_x = average_rps_x * frame.wheel_circumference ; // m/s
     
     average_rps_y = ((float)(-rpm1 + rpm2 ) / 2) / 60; // RPM
     vel.linear_y = 0;
 
     //convert average revolutions per minute to revolutions per second
-    average_rps_a = ((float)(-rpm1 + rpm2 ) / total_wheels_) / 60;
-    vel.angular_z =  (average_rps_a * wheel_circumference_) / ((wheels_x_distance_ / 2) + (wheels_y_distance_ / 2)); //  rad/s
+    average_rps_a = ((float)(-rpm1 + rpm2 ) / frame.total_wheels) / 60;
+    vel.angular_z =  (average_rps_a * frame.wheel_circumference) / ((frame.wheels_x_distance / 2) + (frame.wheels_y_distance / 2)); //  rad/s
     return vel;
 }
 
-
+/*------------------------------------------------------------------------
+ * 
+ * 
+ -------------------------------------------------------------------------*/
 
 int computePid( double targetValue, double currentValue) 
 {
+  struct PID pid ;
   double pidTerm = 0;                            // PID correction
   double error = 0;
   double new_PWM = 0;
@@ -234,7 +242,7 @@ int computePid( double targetValue, double currentValue)
   error = targetValue-currentValue;
   int_Error += error;
     
-  pidTerm = Kp*error + Ki*int_Error + Kd*(error-last_Error) ;
+  pidTerm = pid.Kp*error + pid.Ki*int_Error + pid.Kd*(error-last_Error) ;
   last_Error = error;
   
   new_PWM = constrain( pidTerm, -MAX_RPM, MAX_RPM);
@@ -242,35 +250,110 @@ int computePid( double targetValue, double currentValue)
   return new_PWM;
 }
 
-
-int get_current_RPM(   long encoder_ticks )
+/*------------------------------------------------------------------------
+ * 
+ * 
+ -------------------------------------------------------------------------*/
+int get_current_RPM(   long  current_encoder_ticks, int counts_per_rev )
 {
 
     //this function calculates the motor's RPM based on encoder ticks and delta time
-    unsigned long current_time = millis();
-    unsigned long dt = current_time - prev_update_time_;
+    unsigned long current_encoder_time = millis();
+    unsigned long dt = current_encoder_time - prev_encoder_time;
 
     //convert the time from milliseconds to minutes
     double dtm = (double)dt / 60000;
-    double delta_ticks = encoder_ticks - prev_encoder_ticks_;
+    double delta_ticks = current_encoder_ticks - prev_encoder_ticks;
 
     //calculate wheel's speed (RPM)
-    prev_update_time_ = current_time;
-    prev_encoder_ticks_ = encoder_ticks;
+    prev_encoder_time = current_encoder_time;
+    prev_encoder_ticks =  current_encoder_ticks;
     
-    return (delta_ticks / counts_per_rev_) / dtm;
+    return (delta_ticks / counts_per_rev) / dtm;
   }
 
+/*------------------------------------------------------------------------
+ * 
+ * 
+ -------------------------------------------------------------------------*/  
+void  controllerPort ( int int_motor )
+{
+    int   motor_pinA;
+    int   motor_pinB;
+    int   pwm_pin;
+  switch (int_motor)
+    {
+      case 1:
+      motor_pinA ;
+      motor_pinB ;
+      pwm_pin =0 ;      
+      break;
+
+      case 2: 
+      motor_pinA ;
+      motor_pinB ;
+      pwm_pin ;
+      break;
+    }
+    pinMode(pwm_pin, OUTPUT);
+    pinMode(motor_pinA, OUTPUT);
+    pinMode(motor_pinB, OUTPUT);
+
+    //ensure that the motor is in neutral state during bootup
+   analogWrite(pwm_pin, abs(0));
+}
+
+/*------------------------------------------------------------------------
+ * 
+ * 
+ -------------------------------------------------------------------------*/
+void motorSpin ( int int_motor,int pwm)
+{
+    int   motor_pinA  ;
+    int   motor_pinB ;
+    int   pwm_pin ;
+      
+    switch (int_motor)
+    {
+      case 1:
+       motor_pinA = 1 ;
+       motor_pinB  = 2 ;
+       pwm_pin = 0;
+      break;
+
+      case 2: 
+       motor_pinA = 3 ;
+       motor_pinB = 4 ;
+       pwm_pin = 5 ;
+       
+      break;
+    }
+            if(pwm > 0)
+            {
+                digitalWrite(motor_pinA, HIGH);
+                digitalWrite(motor_pinB, LOW);
+            }
+            else if(pwm < 0)
+            {
+                digitalWrite(motor_pinA, LOW);
+                digitalWrite(motor_pinB, HIGH);
+            }
+            analogWrite(pwm_pin, abs(pwm));
+
+}// motor spin
+
+/*------------------------------------------------------------------------
+ * 
+ * 
+ -------------------------------------------------------------------------*/
 void moveBase()
 {
- // kinematics.getRPM(g_req_linear_vel_x, g_req_linear_vel_y, g_req_angular_vel_z);
 
-// rpm =IF_kinematics(g_req_linear_vel_x, g_req_linear_vel_y, g_req_angular_vel_z);
  IF_kinematics_RPM(g_req_linear_vel_x, g_req_linear_vel_y, g_req_angular_vel_z);
-     struct pwm pwm;
-     struct rpm req_rpm;
-    int current_rpm1 =  get_current_RPM (encoder1.read());
-    int current_rpm2 =  get_current_RPM (encoder2.read());
+    struct pwm pwm;
+    struct rpm req_rpm;
+    int current_rpm1 =  get_current_RPM (encoder1.read(),400);
+    int current_rpm2 =  get_current_RPM (encoder2.read(),400);
 
     //the required rpm is capped at -/+ MAX_RPM to prevent the PID from having too much error
     //the PWM value sent to the motor driver is the calculated PID based on required RPM vs measured RPM
@@ -278,12 +361,11 @@ void moveBase()
     pwm.motor1 = computePid(req_rpm.motor1, current_rpm1);
     pwm.motor2 = computePid(req_rpm.motor2, current_rpm2);
 
-//    motor1_controller.spin(pwm.motor1);
- //   motor2_controller.spin(pwm.motor2);
+   motorSpin(1,pwm.motor1);
+   motorSpin(2,pwm.motor2);
    
-
-     velocities current_vel;
-    current_vel = cmd_to_twist_VEL(current_rpm1, current_rpm2);
+   velocities current_vel;
+   current_vel = cmd_to_twist_VEL(current_rpm1, current_rpm2);
     
     //pass velocities to publisher object
    // real_vel_msg.x = current_vel.linear_x;
@@ -291,16 +373,22 @@ void moveBase()
    // real_vel_msg.z = current_vel.angular_z;
 
     //publish raw_vel_msg
-      rpm_pub.publish(&real_vel_msg);
+   rpm_pub.publish(&real_vel_msg);
 }
-
+/*------------------------------------------------------------------------
+ * 
+ * 
+ -------------------------------------------------------------------------*/
 void stopBase()
 {
     g_req_linear_vel_x = 0;
     g_req_linear_vel_y = 0;
     g_req_angular_vel_z = 0;
 }
-
+/*------------------------------------------------------------------------
+ * 
+ * 
+ -------------------------------------------------------------------------*/
 void printDebug(boolean DEBUG)
 {
     char buffer[50];
@@ -318,36 +406,3 @@ void printDebug(boolean DEBUG)
      }  
    
 }
-
-void InitMotor ()
-{
-  
-}
-
-port  controllerPort ( int pwm_pin_, int motor_pinA_, int motor_pinB_ )
-{
-            pinMode(pwm_pin_, OUTPUT);
-            pinMode(motor_pinA_, OUTPUT);
-            pinMode(motor_pinB_, OUTPUT);
-
-            //ensure that the motor is in neutral state during bootup
-            analogWrite(pwm_pin_, abs(0));
-}
-
-/*
-void Motor (int pwm)
-{
-    
-            if(pwm > 0)
-            {
-                digitalWrite(motor_pinA_, HIGH);
-                digitalWrite(motor_pinB_, LOW);
-            }
-            else if(pwm < 0)
-            {
-                digitalWrite(motor_pinA_, LOW);
-                digitalWrite(motor_pinB_, HIGH);
-            }
-            analogWrite(pwm_pin_, abs(pwm));
-
-    }
