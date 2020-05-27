6b4c639adefc7ba99a377ed6a795cbb46ecfeee5
Christopher Coballes <cobecoballes@gmail.com>
2020-05-27 06:09:00 -0400

    Update base_controller.cpp

--- src/base_controller.cpp
+++ src/base_controller.cpp
@@ -2,11 +2,18 @@
 #include <nav_msgs/Odometry.h>
 #include <tf2_ros/transform_broadcaster.h>
 #include <tf2/LinearMath/Quaternion.h>
+#include <geometry_msgs/Twist.h>
 #include <geometry_msgs/TransformStamped.h>
 
-double  linear_velocity_x ;
-double  linear_velocity_y ;
-double  angular_velocity_z;
+double  linear_velocity_x =  0 ;
+double  linear_velocity_y =  0 ;
+double  angular_velocity_z = 0 ;
+
+double  last_vel_time = 0;
+double  vel_dt        = 0;
+double  x_pos         = 0;
+double  y_pos         = 0;
+double  heading       = 0;  // robot directions
 
 void commandCallback(const geometry_msgs::Twist&  vel)
 {
@@ -22,7 +29,8 @@ int main(int argc, char** argv){
 
     ros::NodeHandle n;
     ros::Publisher odom_pub = n.advertise<nav_msgs::Odometry>("odom", 10);
-    ros::Subscriber<geometry_msgs::Twist> cmd_sub("cmd_vel", commandCallback);
+    //ros::Subscriber<geometry_msgs::Twist> cmd_sub("cmd_vel", commandCallback);
+    ros::Subscriber sub = n.subscribe(""raw_vel"", 10, handle_rpm);
   
     tf2_ros::TransformBroadcaster odom_broadcaster;
 
@@ -40,64 +48,78 @@ int main(int argc, char** argv){
 
     ros::Rate r(50.0);
 
-    while(n.ok()){
+    while(n.ok())
+    {
 
-        ros::spinOnce();
-        current_time = ros::Time::now();
+    ros::spinOnce();
+    ros::Time current_time = ros::Time::now();
 
-        double dt = (current_time-last_time).toSec();
-        double delta_x = (vx * cos(th) - vy * sin(th)) * dt;
-        double delta_y = (vx * sin(th) + vy * cos(th)) * dt;
-        double delta_th = vth * dt;
+    linear_velocity_x_ = vel.linear_x;
+    linear_velocity_y_ = vel.linear_y;
+    angular_velocity_z_ = vel.angular_z;
 
-        x += delta_x;
-        y += delta_y;
-        th += delta_th;
+    vel_dt_ = (current_time - last_vel_time_).toSec();
+    last_vel_time_ = current_time;
 
-        // Setting the transform stamped messages:
-        tf2::Quaternion q;
-        q.setRPY(0,0,th);
-        geometry_msgs::TransformStamped odom_trans;
-        odom_trans.header.stamp = current_time;
-        odom_trans.header.frame_id = "odom";
-        odom_trans.child_frame_id = "base_link";
+    double delta_heading = angular_velocity_z_ * vel_dt_; //radians
+    double delta_x = (linear_velocity_x_ * cos(heading_) - linear_velocity_y_ * sin(heading_)) * vel_dt_; //m
+    double delta_y = (linear_velocity_x_ * sin(heading_) + linear_velocity_y_ * cos(heading_)) * vel_dt_; //m
 
-        odom_trans.transform.translation.x = x;
-        odom_trans.transform.translation.y = y;
-        odom_trans.transform.translation.z = 0.0;
-        odom_trans.transform.rotation.x = q.x();
-        odom_trans.transform.rotation.y = q.y();
-        odom_trans.transform.rotation.z = q.z();
-        odom_trans.transform.rotation.w = q.w();
+    //calculate current position of the robot
+    x_pos_ += delta_x;
+    y_pos_ += delta_y;
+    heading_ += delta_heading;
 
-        // Send the transform:
-        odom_broadcaster.sendTransform(odom_trans);
+    //calculate robot's heading in quaternion angle
+    //ROS has a function to calculate yaw in quaternion angle
+    odom_quat.setRPY(0,0,heading_);
 
-        // Send the odometry messages to ROS:
-        nav_msgs::Odometry odom;
-        odom.header.stamp = current_time;
-        odom.header.frame_id = "odom";
-        // Here, take the positions from the wheelme backend whenever it reaches and send it to ros:
-        odom.pose.pose.position.x = x;
-        odom.pose.pose.position.y = y;
-        odom.pose.pose.position.z = 0.0;
-        odom.pose.pose.orientation.x = q.x();
-        odom.pose.pose.orientation.x = q.y();
-        odom.pose.pose.orientation.x = q.z();
-        odom.pose.pose.orientation.x = q.w();
+    odom_trans.header.frame_id = "odom";
+    odom_trans.child_frame_id = "base_footprint";
+    //robot's position in x,y, and z
+    odom_trans.transform.translation.x = x_pos_;
+    odom_trans.transform.translation.y = y_pos_;
+    odom_trans.transform.translation.z = 0.0;
+    //robot's heading in quaternion
+    odom_trans.transform.rotation.x = odom_quat.x();
+    odom_trans.transform.rotation.y = odom_quat.y();
+    odom_trans.transform.rotation.z = odom_quat.z();
+    odom_trans.transform.rotation.w = odom_quat.w();
+    odom_trans.header.stamp = current_time;
+    //publish robot's tf using odom_trans object
+    //odom_broadcaster_.sendTransform(odom_trans);
 
+    odom.header.stamp = current_time;
+    odom.header.frame_id = "odom";
+    odom.child_frame_id = "base_footprint";
 
-        // This values will be always zero until wheelme will start publishing its velocities:
-        odom.child_frame_id = "base_link";
-        odom.twist.twist.linear.x = vx;
-        odom.twist.twist.linear.y = vy;
-        odom.twist.twist.angular.z  = vth;
+    //robot's position in x,y, and z
+    odom.pose.pose.position.x = x_pos_;
+    odom.pose.pose.position.y = y_pos_;
+    odom.pose.pose.position.z = 0.0;
+    //robot's heading in quaternion
+    odom.pose.pose.orientation.x = odom_quat.x();
+    odom.pose.pose.orientation.y = odom_quat.y();
+    odom.pose.pose.orientation.z = odom_quat.z();
+    odom.pose.pose.orientation.w = odom_quat.w();
+    odom.pose.covariance[0] = 0.001;
+    odom.pose.covariance[7] = 0.001;
+    odom.pose.covariance[35] = 0.001;
 
-        // Now publish all that to odom topic:
-        odom_pub.publish(odom);
-        last_time = current_time;
-        r.sleep();
+    //linear speed from encoders
+    odom.twist.twist.linear.x = linear_velocity_x_;
+    odom.twist.twist.linear.y = linear_velocity_y_;
+    odom.twist.twist.linear.z = 0.0;
 
+    odom.twist.twist.angular.x = 0.0;
+    odom.twist.twist.angular.y = 0.0;
+    //angular speed from encoders
+    odom.twist.twist.angular.z = angular_velocity_z_;
+    odom.twist.covariance[0] = 0.0001;
+    odom.twist.covariance[7] = 0.0001;
+    odom.twist.covariance[35] = 0.0001;
+
+    odom_publisher_.publish(odom);
     }
 
 }
